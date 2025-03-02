32606f1287d9799af03368f88c086c870d1208d1
cekharchandra-devops <cekharchandra.devops@gmail.com>
2025-03-03 03:34:44 +0530

    consistent infra across dev and prod env poc using 3-tier application completed with tfvars

--- main.tf
+++ main.tf
@@ -2,4 +2,5 @@ resource "aws_instance" "ec2" {
   ami = var.ami_id
   instance_type = var.instance_type
   tags = var.tags
+  vpc_security_group_ids = var.sg_ids
 }
--- variables.tf
+++ variables.tf
@@ -10,6 +10,20 @@ variable "tags" {
 }
 
 variable "instance_type" {
-  default = "t2.micro"
+  default = "t3.micro"
+  type = string
+  validation {
+    condition = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
+    error_message = "instance_type must be either t3.micro, t3.small, or t3.medium"
+  }
+}
+
+variable "sg_ids" {
+  type = list(string)
+  default = ["sg-0158e0bf4b6d8891e"]
+  validation {
+    condition = length(var.sg_ids) > 0
+    error_message = "sg_ids must have at least one value"
+  }
   
 }
